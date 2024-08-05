f2961fcb5a20ee858e91730a33665c7b5e6b03a7
cronquillof <88749701+cronquillof@users.noreply.github.com>
2024-08-05 12:00:54 -0500

    funiber logo + app js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [inputSound, setInputSound] = useState('');
+  const [result, setResult] = useState('');
+
+  const songs = [
+    ["brr", "fiu", "cric-cric", "brrah"],
+    ["pep", "birip", "trri-trri", "croac"],
+    ["bri-bri", "plop", "cric-cric", "brrah"]
+  ];
+
+  const findRemainingSounds = () => {
+    let remainingSounds = '';
+
+    for (let song of songs) {
+      if (song.includes(inputSound)) {
+        const index = song.indexOf(inputSound);
+        remainingSounds = song.slice(index + 1).join(", ");
+        break;
+      }
+    }
+
+    setResult(remainingSounds ? remainingSounds : 'No hay sonidos restantes.');
+  };
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <img src={'funiber-400.jpg'} className="App-logo" alt="logo" />
+      <h1>Explorador de Sonidos</h1>
+      <input
+        type="text"
+        value={inputSound}
+        onChange={(e) => setInputSound(e.target.value)}
+        placeholder="Ingresa un sonido"
+      />
+      <button onClick={findRemainingSounds}>Buscar</button>
+      <div id="result">{result}</div>
     </div>
   );
 }
